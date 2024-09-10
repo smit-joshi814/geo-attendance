ffad2b217db12de7ecd49233b9b5abee35d4c4ac
Smit Joshi <smitjoshi814@gmail.com>
2024-09-10 23:15:32 +0530

    Initial Setup

---
+++ src/main/java/com/geo/attendance/GeoAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.geo.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geo/attendance/GeoAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
